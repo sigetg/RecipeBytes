52e0ea15fb1945893727804cf4a04a2f09d1ed81
Aimee Hong <honglp@bc.edu>
2024-11-19 14:05:27 -0500

    HTML and CSS for Grocery-list

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './App.css';
 import NavigationBar from './components/nav';
 import RecipeList from './components/Recipe-list';
+import GroceryList from './components/Grocery-list';
 
 function App() {
   return (
     <div className="App">
       <NavigationBar />
-      <RecipeList />
     </div>
   );
 }
