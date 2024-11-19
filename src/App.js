d6f7ef7fd7b6e9c85ccf84493642f4ae14f943be
anorazhu <zhusm@bc.edu>
2024-11-19 14:05:12 -0500

    recipe list search bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import NavigationBar from './components/nav';
+import RecipeList from './components/Recipe-list';
 
 function App() {
   return (
     <div className="App">
       <NavigationBar />
+      <RecipeList />
     </div>
   );
 }
