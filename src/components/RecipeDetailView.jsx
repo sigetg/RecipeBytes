10d1e04b41a7a50bc55c6f6c13b8c69074c9eb1a
anorazhu <zhusm@bc.edu>
2024-11-20 02:00:38 -0500

    recipe detail view - added ingredients

--- src/components/RecipeDetailView.jsx
+++ src/components/RecipeDetailView.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { Box, Typography } from "@mui/material";
 import { recipeData } from "../data/recipeData";
+import { ingredients } from "../data/ingredients";
+import { IngredientsIcon } from "../assets/icons";
 
 export default function RecipeDetailView() {
-  const { title } = useParams(); // Get the recipe title from the URL
+  const { title } = useParams(); 
   const recipe = recipeData.find(
     (r) => r.title === decodeURIComponent(title)
   );
@@ -17,29 +19,43 @@ export default function RecipeDetailView() {
     );
   }
 
+  const isInPantry = (ingredientName) => {
+    return ingredients.some((pantryItem) =>
+      pantryItem.name.toLowerCase() === ingredientName.toLowerCase()
+    );
+  };
+
   return (
     <Box sx={{ padding: "30px" }}>
-      <Typography variant="h4" sx={{ marginBottom: "20px" }}>
+      <Typography variant="h4" sx={{ marginBottom: "20px" }} >
         {recipe.title}
       </Typography>
-      <Typography variant="h6" sx={{ marginBottom: "10px" }}>
+      <Typography variant="h4" sx={{ marginBottom: "10px", fontFamily: "'Patrick Hand SC', cursive", }}>
+      <Box sx={{ display: "flex", alignItems: "center", gap: "10px", justifyContent: "center" }}>
+        <IngredientsIcon />
         Ingredients:
+      </Box>
       </Typography>
-      <ul>
+      <ul style={{ listStyleType: "none"}}>
         {recipe.ingredients.map((item, index) => (
           <li key={index}>
+            <input
+              type="checkbox"
+              checked={isInPantry(item.name)}
+              disabled
+            />
             {item.quantity} {item.name}
           </li>
         ))}
       </ul>
-      <Typography variant="h6" sx={{ marginTop: "20px", marginBottom: "10px" }}>
+      {/* <Typography variant="h6" sx={{ marginTop: "20px", marginBottom: "10px" }}>
         Instructions:
       </Typography>
       <ol>
         {recipe.instructions.map((step, index) => (
           <li key={index}>{step}</li>
         ))}
-      </ol>
+      </ol> */}
     </Box>
   );
 }
