5957d6f988b4765e2460de460385f91eb6508e9b
anorazhu <zhusm@bc.edu>
2024-11-20 02:49:26 -0500

    recipe detail updated

--- src/components/RecipeDetailView.jsx
+++ src/components/RecipeDetailView.jsx
@@ -3,7 +3,8 @@ import { useParams } from "react-router-dom";
 import { Box, Typography } from "@mui/material";
 import { recipeData } from "../data/recipeData";
 import { ingredients } from "../data/ingredients";
-import { IngredientsIcon } from "../assets/icons";
+import { IngredientsIcon, TipsIcon, SubstitutionIcon } from "../assets/icons";
+import "../styles/RecipeDetails.css"
 
 export default function RecipeDetailView() {
   const { title } = useParams(); 
@@ -27,27 +28,50 @@ export default function RecipeDetailView() {
 
   return (
     <Box sx={{ padding: "30px" }}>
-      <Typography variant="h4" sx={{ marginBottom: "20px" }} >
+      <Typography variant="h4" sx={{ marginBottom: "20px", fontFamily: "'Patrick Hand SC', cursive", paddingBottom: "10px" }} >
         {recipe.title}
       </Typography>
-      <Typography variant="h4" sx={{ marginBottom: "10px", fontFamily: "'Patrick Hand SC', cursive", }}>
-      <Box sx={{ display: "flex", alignItems: "center", gap: "10px", justifyContent: "center" }}>
-        <IngredientsIcon />
-        Ingredients:
-      </Box>
-      </Typography>
-      <ul style={{ listStyleType: "none"}}>
-        {recipe.ingredients.map((item, index) => (
-          <li key={index}>
-            <input
-              type="checkbox"
-              checked={isInPantry(item.name)}
-              disabled
-            />
-            {item.quantity} {item.name}
-          </li>
-        ))}
-      </ul>
+      <div className="container-content">
+        <div id="ingredient-container">
+            <Typography variant="h4" sx={{ marginBottom: "10px", fontFamily: "'Patrick Hand SC', cursive", }}>
+                <Box sx={{ display: "flex", alignItems: "center", gap: "10px", justifyContent: "center", padding: "10px 0" }}>
+                    <IngredientsIcon />
+                    Ingredients
+                </Box>
+            </Typography>
+            <ul style={{ listStyleType: "none"}}>
+                {recipe.ingredients.map((item, index) => (
+                <li key={index}>
+                    <input
+                    type="checkbox"
+                    checked={isInPantry(item.name)}
+                    disabled
+                    />
+                    <div>
+                        {item.quantity} {item.name}
+                    </div>
+                </li>
+                ))}
+            </ul>
+        </div>
+        <div id="recipe-tips-container">
+        <Typography variant="h4" sx={{ marginBottom: "10px", fontFamily: "'Patrick Hand SC', cursive", }}>
+                <Box sx={{ display: "flex", alignItems: "center", gap: "10px", justifyContent: "center",  padding: "10px 0" }}>
+                    <TipsIcon />
+                    Recipe Tips
+                </Box>
+            </Typography>
+        </div>
+        <div id="substitutes-container">
+        <Typography variant="h4" sx={{ marginBottom: "10px", fontFamily: "'Patrick Hand SC', cursive", }}>
+                <Box sx={{ display: "flex", alignItems: "center", gap: "10px", justifyContent: "center",  padding: "10px 0" }}>
+                    <SubstitutionIcon />
+                    Substitutes
+                </Box>
+            </Typography>
+        </div>
+      </div>
+      
       {/* <Typography variant="h6" sx={{ marginTop: "20px", marginBottom: "10px" }}>
         Instructions:
       </Typography>
