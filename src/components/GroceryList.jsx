d352462ab822e6648f29b23fb123ed04fa00285e
George Sigety <89882158+sigetg@users.noreply.github.com>
2024-11-26 16:04:41 -0500

    Add data to groceryList

--- src/components/GroceryList.jsx
+++ src/components/GroceryList.jsx
@@ -62,18 +62,116 @@ const GroceryList = () => {
     setIngredients(updatedIngredients);
   };
 
+  const toggleForm = () => {
+    setShowForm(prevShowForm => !prevShowForm);
+  };
+
+// Maybe refactor to implement this later...
+  // const GroceryItem = ({ name }) => (
+  //     <li key={ingredient.id}>
+  //     <input
+  //       type="checkbox"
+  //       name={`${name}${ingredient.id}`}
+  //       value={ingredient.title}
+  //       checked={selectedItems.includes(ingredient.id)}
+  //       onChange={() => handleCheckboxChange(ingredient.id)}
+  //     />
+  //     <label htmlFor={`${name}${ingredient.id}`}>{ingredient.title}</label><br />
+  //   </li>
+  // );
+
+  const GroceryCategory = ({ name, ingredients }) => (
+    <div className={`${css[name.toLowerCase()]} ${(name === "grain" || name === "other") ? css.smallColumn : ""}`}>
+      <h2>{name}</h2>
+      {ingredients.filter(ingredient => ingredient.category === name).length > 0 ? (
+        <ul>
+          {ingredients
+            .filter(ingredient => ingredient.category === name)
+            .map(ingredient => (
+              <li key={ingredient.id}>
+                <input
+                  type="checkbox"
+                  name={`${name}${ingredient.id}`}
+                  value={ingredient.title}
+                  checked={selectedItems.includes(ingredient.id)}
+                  onChange={() => handleCheckboxChange(ingredient.id)}
+                />
+                <label htmlFor={`${name}${ingredient.id}`}>{ingredient.title}</label><br />
+              </li>
+            ))}
+          </ul>
+      ) : (
+        <p>No items yet. Click 'New Item' to add one now!</p>
+      )}
+    </div>
+  );
+
     return (
       <body className="grocery-body">
         <main className={css.container}>
         <div className={css.header}>
           <h1>Groceries</h1>
-          <Button variant="text" href="#outlined-buttons" sx={{fontFamily: "'Patrick Hand SC', cursive", fontSize: 18}}>
+          <Button variant="text" href="#outlined-buttons" onClick={handleDeleteSelected} sx={{fontFamily: "'Patrick Hand SC', cursive", fontSize: 18}}>
+            Delete Selected
+          </Button>
+          <Button variant="text" href="#outlined-buttons" onClick={toggleForm} sx={{fontFamily: "'Patrick Hand SC', cursive", fontSize: 18}}>
             ⨁ New Item
           </Button>
+        {showForm && (
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="title">Title:</label>
+              <input
+                type="text"
+                id="title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="quantity">Quantity:</label>
+            <input
+                type="number"
+                id="quantity"
+                value={quantity}
+                onChange={(e) => setQuantity(Number(e.target.value))}
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="category">Category:</label>
+              <select
+                id="category"
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+                required
+              >
+                <option value="produce">Produce</option>
+                <option value="protein">Protein</option>
+                <option value="dairy">Dairy</option>
+                <option value="grain">Grains</option>
+                <option value="other">Other</option>
+              </select>
+            </div>
+            <div>
+              <label htmlFor="expiration">Expiration Date:</label>
+              <input
+                type="date"
+                id="expiration"
+                value={expiration}
+                onChange={(e) => setExpiration(e.target.value)}
+                required
+              />
+            </div>
+            <button type="submit" className="button">Add Ingredient</button>
+          </form>
+        )}
+
         </div>
         <section className={css.grocery}>
           {groceries.map((category) => (
-              <GroceryCategory key={category.name} name={category.name} items={category.items} />
+              <GroceryCategory key={category.name} name={category.name} ingredients={ingredients} />
             ))}
         </section>
       </main>
@@ -81,26 +179,3 @@ const GroceryList = () => {
     )
 }
 export default GroceryList;
-
-const GroceryCategory = ({ name, items }) => (
-  <div className={`${css[name.toLowerCase()]} ${(name === "grain" || name === "other") ? css.smallColumn : ""}`}>
-    <h2>{name}</h2>
-    {items.length > 0 ? (
-      <ul>
-        {items.map((item, index) => (
-          <GroceryItem key={index} name={item} />
-        ))}
-      </ul>
-    ) : (
-      <p>No items yet. Click 'New Item' to add one now!</p>
-    )}
-  </div>
-);
-
-
-const GroceryItem = ({ name }) => (
-  <li>
-    <input type="checkbox" id={name.toLowerCase()} value={name} />
-    <label htmlFor={name.toLowerCase()}>{name}</label>
-  </li>
-);
