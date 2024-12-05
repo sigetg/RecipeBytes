08b2bfb6323dc67867db726b7f5f35da5f79cef8
Aimee Hong <honglp@bc.edu>
2024-12-05 14:27:06 -0500

    made home page styling more consistent

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -39,73 +39,89 @@ export default function Pantry() {
     setShowForm(false);
   };
 
-  return (
-    <main className={css.container}>
-      <section className={css.profileAvatar}>
-        <img src={photoURL} alt="Avatar" />
-      </section>
-      <section className={css.accountInfo}>
-        <div className={css.name}>
-          <label htmlFor="name">Name:</label>
-          <br />
-          <input id={userId} name="name" value={user.displayName} disabled />
-        </div>
-        <div className={css.email}>
-          <label htmlFor="email">Email:</label>
-          <br />
-          <input id={userId} name="email" value={user.email} disabled />
-        </div>
-      </section>
-      <section>
-        {showForm && (
-          <form onSubmit={handleSubmit} className={css.form}>
-            <h2>Update Profile</h2>
+  // const handleChange = async (e) => {
+  //   if (e.target.files[0]) {
+  //     setPhoto(e.target.files[0]);
+  //   }
+  //   uploadProfileImage(photo, setLoading).then((url) => {
+  //     setPhotoURL(url);
+  //   });
+  // };
+
+//   // storage for user's profile image
+//  async function uploadProfileImage(file, userId, setLoading) {
+//   const storageRef = ref(storage, `profileImages/${userId}`);
+//   setLoading(true);
+//   await uploadBytes(storageRef, file);
+//   setLoading(false);
+//   return getDownloadURL(storageRef);
+// }
+
+  return <main className={css.container}>
+    <section className={css.profileAvatar}>
+      <img
+        src={photoURL}
+        alt="Avatar"
+      />
+    </section>
+    <section className={css.accountInfo}>
+      <div className={css.name}>
+        <label for="name">Name:</label><br />
+        <input
+          id={userId}
+          name="name"
+          value={user.displayName} 
+          disabled
+        />
+      </div>
+      <div className={css.email}>
+        <label for="name">Email:</label><br />
+        <input
+          id={userId}
+          name="email"
+          value={user.email}
+          disabled
+        />
+      </div>
+    </section>
+    <section>
+      {showForm && (
+            <form onSubmit={handleSubmit} className={css.form}>
+              <h2>Update Profile</h2>
+              <TextField
+                label="First Name"
+                variant="outlined"
+                margin="normal"
+                onChange={(e) => setFirstName(e.target.value)}
+                type="text"
+              />
             <TextField
-              label="First Name"
-              variant="outlined"
-              margin="normal"
-              onChange={(e) => setFirstName(e.target.value)}
-              type="text"
+                label="Last Name"
+                variant="outlined"
+                margin="normal"
+                onChange={(e) => setLastName(e.target.value)}
+                type="text"
             />
             <TextField
-              label="Last Name"
-              variant="outlined"
-              margin="normal"
-              onChange={(e) => setLastName(e.target.value)}
-              type="text"
+                variant="outlined"
+                margin="normal"
+                onChange={(e) => setPhotoURL(e.target.value)}
+                // onClick={handleChange}
+                type="file"
             />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              onChange={(e) => setPhotoURL(e.target.value)}
-              type="file"
-            />
-            <button type="submit" className={css.profileUpdateButton}>
-              Save
-            </button>
-          </form>
-        )}
-        <section className={css.options}>
-          <Stack direction="row" spacing={2}>
-            <Button
-              variant="text"
-              onClick={toggleForm}
-              sx={{ fontFamily: "'Patrick Hand SC', cursive", fontSize: 18, border: 1 }}
-            >
-              Update Profile
-            </Button>
-            {user && (
-              <Button
-                variant="text"
-                onClick={handleLogout}
-                sx={{ fontFamily: "'Patrick Hand SC', cursive", fontSize: 18, border: 1 }}
-              >
-                Logout
-              </Button>
-            )}
-          </Stack>
-        </section>
+            <button type="submit" className={css.profileUpdateButton}>Save</button>
+            </form>
+          )}
+      <section className={css.options}>
+        <Stack direction="row" spacing={2}>
+          <Button variant="text" onClick={toggleForm} sx={{fontFamily: "'Patrick Hand SC', cursive", fontSize: 18, border: 1}}>
+            Update Profile
+          </Button>
+          {user && ( <Button variant="text" onClick={handleLogout} sx={{fontFamily: "'Patrick Hand SC', cursive", fontSize: 18, border: 1}}>
+            Logout
+          </Button> )}
+        </Stack>
       </section>
-    </main>
-  );
+    </section>
+  </main>
 }
